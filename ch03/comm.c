2dc6203d3bcea33b809673c448d4fe28ba0efeee
Chaitanya Kumar <chaitanyak28771@gmail.com>
2021-01-22 16:59:09 +0530

    initial commit

---
+++ ch03/comm.c
@@ -0,0 +1,8 @@
+int MPI_Comm_size(
+	MPI_Comm comm	/* in */,
+	int* comm_sz_p  /* out */)
+
+int MPI_Comm_rank(
+	MPI_Comm comm	/* in */,
+	int* my_rank_p	/* out */)
+
---
+++ indetermin.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <mpi.h>
+
+int main(void) {
+	int my_rank, comm_sz;
+
+	MPI_Init(NULL, NULL);
+	MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+	printf("Proc %d of %d\n", my_rank, comm_sz);
+
+	MPI_Finalize();
+
+	return 0;
+}
